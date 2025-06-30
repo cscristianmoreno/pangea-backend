d85f5dfdb74f6e823005e22214179761c5e2a9c5
cscristian <welcome_salta@hotmail.com>
2025-06-30 18:07:45 -0300

    Añadido compilación y subida a instancia EC2

--- src/main/java/com/backend/pangea/security/CustomAuthenticationManager.java
+++ src/main/java/com/backend/pangea/security/CustomAuthenticationManager.java
@@ -1,7 +1,6 @@
 package com.backend.pangea.security;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContext;
